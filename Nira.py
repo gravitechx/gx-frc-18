679b66cdc4d4e7d71f60fefee3198836521efd9c
Nirajara <32470269+Nirajara@users.noreply.github.com>
2018-02-16 17:08:51 -0800

    Update Nira.py
    Decently accurate distance method and degrees needed to turn

--- Nira.py
+++ Nira.py
@@ -2,7 +2,6 @@ import numpy as np
 import cv2
 import math
 
-
 CLOSE_AREA  = 233452.5
 
 #yellow color ranges
@@ -10,7 +9,7 @@ lower_yellow = np.array([27, 50, 145])
 upper_yellow = np.array([40, 255, 255])
     
 #image used in program
-im = cv2.imread('C:/Users/GravitechX/Desktop/Boxes.jpg', 1)
+im = cv2.imread('C:/Users/GravitechX/Desktop/10ft.jpg', 1)
 
 #changing image colorspace from BGR to HSV
 hsv = cv2.cvtColor(im, cv2.COLOR_BGR2HSV)
@@ -117,7 +116,7 @@ distance_from_bottom = height - cy
 cv2.circle(final, (cx, cy), 1, (0,255,0), thickness=1, lineType=8, shift=0)
 
 #draws box contour onto hsv and im
-hsv = cv2.drawContours(hsv, contours[y], -1, (0, 0, 255), 5)
+#hsv = cv2.drawContours(hsv, contours[y], -1, (0, 0, 255), 5)
 im = cv2.drawContours(im, contours[y], -1, (0, 0, 255), 5)
 
 #finds height, width, and other of contour
@@ -136,24 +135,43 @@ else:
 xCenter = (int(width/2))
 yCenter = (int(height/2))
 cv2.circle(im, (xCenter, cy), 1, (255,0,0), thickness=10, lineType=8, shift=0)
-distance = cx - int(width/2)
 
 distance_to_center = xCenter - cx
-print (distance_to_center)
 
-a = width/2
-print (a)
-
-BoxRImage = w/width
-print (BoxRImage)
-
-    
 #draws the centroid onto im
 cv2.circle(im, (cx, cy), 1, (0,255,0), thickness=10, lineType=8, shift=0)
 
-varX = 1.878472
-distance = (width * varX) / w
-print (distance)
+ratio = width/w
+print("RATIO: %s" %ratio)
+
+#Using Width Ratio to find distance and degrees
+WvarX = 1.575778547
+WoriImage = 289
+Wfactor = (width/WoriImage) * WvarX
+print ("factor: %s" %Wfactor)
+Wdistance = (width/w) * Wfactor
+#Wdistance *= 0.3048
+print ("%s meters" %Wdistance)
+
+WdistToCent = (distance_to_center * ((13/12)/w))
+
+Wdegree = -1*((WdistToCent/(2*math.pi*Wdistance))*360)
+print ("Degree: %s" %Wdegree)
+
+#Using Height Ratio
+#HvarX = .9287109375
+#HoriImage = 512
+#Hfactor = (height/HoriImage) * HvarX
+#print ("factor: %s" %Hfactor)
+#Hdistance = (height/h) * Hfactor
+#distance *= 0.3048
+#print ("%s meters" %Hdistance)
+
+#HdistToCent = (distance_to_center * ((13/12)/h))
+
+#Hdegree = (HdistToCent/(2*math.pi*Hdistance))*360
+#print ("Degree: %s" %Hdegree)
+
 
 
 #this is necessary for the instantiation of the vision DO NOT DELETE OR ELSE EVERYTHING WILL BREAK
