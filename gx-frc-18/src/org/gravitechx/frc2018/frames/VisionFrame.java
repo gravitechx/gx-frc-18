9dcbe99bf8e250f51d0727d801b51648b3d6af3e
Katie Seidl <katie.seidl@comcast.net>
2018-03-20 16:35:11 -0700

    added move vision frames and fixed server stuff

--- gx-frc-18/src/org/gravitechx/frc2018/frames/VisionFrame.java
+++ gx-frc-18/src/org/gravitechx/frc2018/frames/VisionFrame.java
@@ -6,8 +6,11 @@ import javax.json.stream.JsonGenerator;
 public class VisionFrame extends Frame {
     private double boxOffset;
     private double boxDistance;
+    private double boxAngle;
     private double tapeOffset;
     private double tapeDistance;
+    private double tapeAngle;
+
 
     /*------------------------------------------------------------.
     |          T I M E S T A M P    C O N S T R U C T O R         |
@@ -21,14 +24,17 @@ public class VisionFrame extends Frame {
         super(baseObject);
         boxOffset = baseObject.getJsonNumber("BOX_OFFSET").doubleValue();
         boxDistance = baseObject.getJsonNumber("BOX_DISTANCE").doubleValue();
+        boxAngle = baseObject.getJsonNumber("BOX_ANGLE").doubleValue();
         tapeOffset = baseObject.getJsonNumber("TAPE_OFFSET").doubleValue();
         tapeDistance = baseObject.getJsonNumber("TAPE_DISTANCE").doubleValue();
-        System.out.println( boxOffset + ", " + boxDistance + ", " + tapeOffset+ ", " + tapeDistance);
+        tapeAngle = baseObject.getJsonNumber("TAPE_ANGLE").doubleValue();
+
+        System.out.println( boxOffset + ", " + boxDistance + ", " + boxAngle + ", " + tapeOffset+ ", " + tapeDistance + ", " + tapeAngle);
     }
     @Override
     protected JsonGenerator encode(JsonGenerator generator) {
         return generator.write("BOX_OFFSET", boxOffset)
-                .write("BOX_DISTANCE", boxDistance).write("TAPE_OFFSET", tapeOffset).write("TAPE_DISTANCE", tapeDistance);
+                .write("BOX_DISTANCE", boxDistance).write("BOX_ANGLE", boxAngle).write("TAPE_OFFSET", tapeOffset).write("TAPE_DISTANCE", tapeDistance).write("TAPE_ANGLE", tapeAngle);
     }
 
     public double getBoxOffset() {
@@ -37,12 +43,20 @@ public class VisionFrame extends Frame {
     public double getBoxDistance(){
         return boxDistance;
     }
+    public double getBoxAngle(){
+        return boxAngle;
+    }
     public double getTapeOffset(){
         return tapeOffset;
     }
     public double getTapeDistance(){
         return tapeDistance;
     }
+    public double getTapeAngle(){
+        return tapeAngle;
+    }
+  //  public VisionInfo getVisionInfo(){ return }
 }
 
 
+//
--- gx-frc-18/src/org/gravitechx/frc2018/robot/io/server/RobotServer.java
+++ gx-frc-18/src/org/gravitechx/frc2018/robot/io/server/RobotServer.java
@@ -1,130 +1,146 @@
 package org.gravitechx.frc2018.robot.io.server;
 
-import org.gravitechx.frc2018.frames.*;
+import org.gravitechx.frc2018.frames.AmpFrame;
+import org.gravitechx.frc2018.frames.Frame;
+import org.gravitechx.frc2018.frames.StatusFrame;
+import org.gravitechx.frc2018.frames.VisionFrame;
 
-import javax.json.Json;
-import javax.json.JsonObject;
-import javax.json.stream.JsonGenerator;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.rmi.server.ExportException;
 import java.util.Stack;
 
 public class RobotServer implements Runnable {
     private boolean mIsRunning;
     private final int UPDATE_TIME;
     private final int PORT;
-    private Stack<Frame> frameStack = new Stack<>();
+    private Stack<Frame> frameStack;
 
     public RobotServer(int port, int update){
         this.PORT = port;
         this.UPDATE_TIME = update;
         mIsRunning = true;
+        frameStack = new Stack<>();
     }
 
     public synchronized void add(Frame frame){
         frameStack.push(frame);
     }
 
-    class FramesSocket implements Runnable{
+    class FramesSocket implements Runnable {
         BufferedReader in;
         PrintWriter out;
         String inputLine;
         Socket clientSocket;
 
 
-        public FramesSocket(Socket clientSocket){
+        public FramesSocket(Socket clientSocket) {
+            this.clientSocket = clientSocket;
 
             try {
-               in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-               out = new PrintWriter(clientSocket.getOutputStream());
-
-            }catch (Exception e){
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                out = new PrintWriter(clientSocket.getOutputStream());
+            } catch (Exception e) {
                 e.printStackTrace();
             }
+
         }
 
         @Override
         public void run() {
-            while (clientSocket.isConnected()) {
+
+            while (true) {
                 try {
                     inputLine = in.readLine();
                 } catch (IOException ioe) {
-                    continue;
+                    ioe.printStackTrace();
                 }
-                System.out.println(inputLine);
-                Frame frame = Frame.toFrame(inputLine);
-                switch (frame.getType()) {
-                    case AMP:
-                        double amp = ((AmpFrame) frame).getAmps();
-                        System.out.println("AMP: " + amp);
-                        break;
-                    case VISION:
-                        double boxOffset = ((VisionFrame) frame).getBoxOffset();
-                        double boxDistance = ((VisionFrame) frame).getBoxDistance();
-                        double tapeOffset = ((VisionFrame) frame).getTapeOffset();
-                        double tapeDistance = ((VisionFrame) frame).getTapeDistance();
-                        System.out.println("BOX_OFFSET:" + boxOffset + ", BOX_DISTANCE:" + boxDistance + ", TAPE_OFFSET:" + tapeOffset + ", TAPE_DISTANCE:" + tapeDistance);
-                        break;
-                    case STATUS:
-                        String msg = ((StatusFrame) frame).getmStatusCode().getMessage();
-                        System.out.println("Message: " + msg);
-                        break;
 
+                if (inputLine != null) {
+                    System.out.println(inputLine);
+
+
+                    Frame frame = Frame.toFrame(inputLine);
+                    switch (frame.getType()) {
+                        case AMP:
+                            double amp = ((AmpFrame) frame).getAmps();
+                            System.out.println("AMP: " + amp);
+                            break;
+                        case VISION:
+                            double boxOffset = ((VisionFrame) frame).getBoxOffset();
+                            double boxDistance = ((VisionFrame) frame).getBoxDistance();
+                            double boxAngle = ((VisionFrame) frame).getBoxAngle();
+                            double tapeOffset = ((VisionFrame) frame).getTapeOffset();
+                            double tapeDistance = ((VisionFrame) frame).getTapeDistance();
+                            double tapeAngle = ((VisionFrame) frame).getTapeAngle();
+                            System.out.println("BOX_OFFSET:" + boxOffset + ", BOX_DISTANCE:" + boxDistance + ", BOX_ANGLE: " + boxAngle + ", TAPE_OFFSET: " + tapeOffset + ", TAPE_DISTANCE: " + tapeDistance + ", TAPE_ANGLE: " + tapeAngle );
+
+                            break;
+                        case STATUS:
+                            String msg = ((StatusFrame) frame).getmStatusCode().getMessage();
+                            System.out.println("Message: " + msg);
+                            break;
+                    }
                 }
+
+                if (clientSocket.isClosed()) {
+                    System.out.println("Server : Bye bye now. (Connection Dropped)");
+                    mIsRunning = false;
+                    break;
+                }
+
+
             }
+            System.out.println("Haha Katie, I've bypassed your puny statements. :D Hahahahahahahaha............");
             try {
                 clientSocket.close();
             } catch (Exception ignored) {
+                ignored.printStackTrace();
             }
         }
     }
-
     @Override
     public void run() {
         System.out.println("You've gotten here!");
-        try(
-                ServerSocket server = new ServerSocket(PORT);
-        ){
-
-            /*
-                    ---- s e n d i n g ----
-            System.out.println("Starting up the server...");
-
-            (new StatusFrame(new LocalTimestamp(), StatusCode.START)).send(out);
-
-            while(mIsRunning){
-                if(!frameStack.isEmpty()){
-                    frameStack.pop().send(out);
-                }
-                System.out.printf("Stack is empty: %b\n", frameStack.isEmpty());
-                Thread.sleep(UPDATE_TIME);
-            }
-            (new StatusFrame(new LocalTimestamp(), StatusCode.STOP)).send(out);
-            System.out.println("Connection with client ended.");
-
-            */
-
+        try {
+            ServerSocket server = new ServerSocket(PORT);
             while (mIsRunning) {
-                System.out.println("wait for client connection...");
-                Socket clientSocket = server.accept();
-                System.out.println("before input!");
-                System.out.println(clientSocket.isConnected());
-
-                // spawn a new thread to handle this client session
-                new Thread(new FramesSocket(clientSocket)).run();
+                try (
+                        Socket clientSocket = server.accept();
+                ) {
+                    System.out.println("before input!");
+                    (new Thread(new FramesSocket(clientSocket))).run();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
-
-
-        }catch(Exception e) {
+        }catch(Exception e){
             e.printStackTrace();
-        }finally{
-
         }
     }
 
     public synchronized void setIsRunning(boolean isRunning){
         this.mIsRunning = isRunning;
     }
+
+                /*
+                ---- s e n d i n g ----
+        System.out.println("Starting up the server...");
+
+        (new StatusFrame(new LocalTimestamp(), StatusCode.START)).send(out);
+
+        while(mIsRunning){
+            if(!frameStack.isEmpty()){
+                frameStack.pop().send(out);
+            }
+            System.out.printf("Stack is empty: %b\n", frameStack.isEmpty());
+            Thread.sleep(UPDATE_TIME);
+        }
+        (new StatusFrame(new LocalTimestamp(), StatusCode.STOP)).send(out);
+        System.out.println("Connection with client ended.");
+
+        */
 }
