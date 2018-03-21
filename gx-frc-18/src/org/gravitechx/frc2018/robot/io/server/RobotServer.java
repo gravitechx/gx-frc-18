ec0d02828d65c554183370c6b6b6f977b4c973c8
Katie Seidl <katie.seidl@comcast.net>
2018-03-21 14:11:24 -0700

    so it doesnt quit if null

--- gx-frc-18/src/org/gravitechx/frc2018/robot/io/server/RobotServer.java
+++ gx-frc-18/src/org/gravitechx/frc2018/robot/io/server/RobotServer.java
@@ -1,6 +1,7 @@
 package org.gravitechx.frc2018.robot.io.server;
 
 import org.gravitechx.frc2018.frames.*;
+import org.gravitechx.frc2018.utils.visionhelpers.VisionInfo;
 
 import javax.json.Json;
 import javax.json.JsonObject;
@@ -35,6 +36,7 @@ public class RobotServer implements Runnable {
 
 
         public FramesSocket(Socket clientSocket){
+            this.clientSocket = clientSocket;
 
             try {
                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
@@ -51,32 +53,45 @@ public class RobotServer implements Runnable {
                 try {
                     inputLine = in.readLine();
                 } catch (IOException ioe) {
+                    ioe.printStackTrace();
                     continue;
                 }
-                System.out.println(inputLine);
-                Frame frame = Frame.toFrame(inputLine);
-                switch (frame.getType()) {
-                    case AMP:
-                        double amp = ((AmpFrame) frame).getAmps();
-                        System.out.println("AMP: " + amp);
-                        break;
-                    case VISION:
-                        double boxOffset = ((VisionFrame) frame).getBoxOffset();
-                        double boxDistance = ((VisionFrame) frame).getBoxDistance();
-                        double tapeOffset = ((VisionFrame) frame).getTapeOffset();
-                        double tapeDistance = ((VisionFrame) frame).getTapeDistance();
-                        System.out.println("BOX_OFFSET:" + boxOffset + ", BOX_DISTANCE:" + boxDistance + ", TAPE_OFFSET:" + tapeOffset + ", TAPE_DISTANCE:" + tapeDistance);
-                        break;
-                    case STATUS:
-                        String msg = ((StatusFrame) frame).getmStatusCode().getMessage();
-                        System.out.println("Message: " + msg);
-                        break;
+                if (inputLine != null) {
+                    System.out.println(inputLine);
+                    Frame frame = Frame.toFrame(inputLine);
+                    switch (frame.getType()) {
+                        case AMP:
+                            double amp = ((AmpFrame) frame).getAmps();
+                            System.out.println("AMP: " + amp);
+                            break;
+                        case VISION:
+                            double boxOffset = ((VisionFrame) frame).getBoxOffset();
+                            double boxDistance = ((VisionFrame) frame).getBoxDistance();
+                            double boxAngle = ((VisionFrame) frame).getBoxAngle();
+                            double tapeOffset = ((VisionFrame) frame).getTapeOffset();
+                            double tapeDistance = ((VisionFrame) frame).getTapeDistance();
+                            double tapeAngle = ((VisionFrame) frame).getTapeAngle();
+                            VisionInfo visionInfo = ((VisionFrame) frame).getVisionInfo();
+                            System.out.println("BOX_OFFSET:" + boxOffset + ", BOX_DISTANCE:" + boxDistance + ", BOX_ANGLE: " + boxAngle + ", TAPE_OFFSET: " + tapeOffset + ", TAPE_DISTANCE: " + tapeDistance + ", TAPE_ANGLE: " + tapeAngle );
+                            break;
+                        case STATUS:
+                            String msg = ((StatusFrame) frame).getmStatusCode().getMessage();
+                            System.out.println("Message: " + msg);
+                            break;
+                    }
+                }
 
+                if (clientSocket.isClosed()) {
+                    System.out.println("Server : Bye bye now. (Connection Dropped)");
+                    mIsRunning = false;
+                    break;
                 }
             }
+            System.out.println("Haha Katie, I've bypassed your puny statements. :D Hahahahahahahaha............");
             try {
                 clientSocket.close();
             } catch (Exception ignored) {
+                ignored.printStackTrace();
             }
         }
     }
@@ -84,47 +99,46 @@ public class RobotServer implements Runnable {
     @Override
     public void run() {
         System.out.println("You've gotten here!");
-        try(
-                ServerSocket server = new ServerSocket(PORT);
-        ){
-
-            /*
-                    ---- s e n d i n g ----
-            System.out.println("Starting up the server...");
-
-            (new StatusFrame(new LocalTimestamp(), StatusCode.START)).send(out);
-
-            while(mIsRunning){
-                if(!frameStack.isEmpty()){
-                    frameStack.pop().send(out);
-                }
-                System.out.printf("Stack is empty: %b\n", frameStack.isEmpty());
-                Thread.sleep(UPDATE_TIME);
-            }
-            (new StatusFrame(new LocalTimestamp(), StatusCode.STOP)).send(out);
-            System.out.println("Connection with client ended.");
-
-            */
+        try {
+            ServerSocket server = new ServerSocket(PORT);
 
             while (mIsRunning) {
-                System.out.println("wait for client connection...");
-                Socket clientSocket = server.accept();
-                System.out.println("before input!");
-                System.out.println(clientSocket.isConnected());
-
-                // spawn a new thread to handle this client session
-                new Thread(new FramesSocket(clientSocket)).run();
+                try (
+                    Socket clientSocket = server.accept();
+                ) {
+                    System.out.println("before input!");
+                    (new Thread(new FramesSocket(clientSocket))).run();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
 
 
         }catch(Exception e) {
             e.printStackTrace();
-        }finally{
-
         }
     }
 
     public synchronized void setIsRunning(boolean isRunning){
         this.mIsRunning = isRunning;
     }
+
+        /*
+               ---- s e n d i n g ----
+        System.out.println("Starting up the server...");
+
+        (new StatusFrame(new LocalTimestamp(), StatusCode.START)).send(out);
+
+        while(mIsRunning){
+            if(!frameStack.isEmpty()){
+                frameStack.pop().send(out);
+            }
+            System.out.printf("Stack is empty: %b\n", frameStack.isEmpty());
+            Thread.sleep(UPDATE_TIME);
+        }
+        (new StatusFrame(new LocalTimestamp(), StatusCode.STOP)).send(out);
+        System.out.println("Connection with client ended.");
+
+        */
+
 }
